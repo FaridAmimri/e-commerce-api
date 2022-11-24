c181e662d6d4ba15707f7e3339b9f1d08a13cdae
Farid Amimri <farid.amimri@gmail.com>
2022-11-24 16:09:26 +0100

    Create Express App + connect Nodejs MongoDB

---
+++ index.js
@@ -0,0 +1,22 @@
+/** @format */
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB Connection Successfull!'))
+  .catch((error) => {
+    console.log(error)
+  })
+
+app.use('/api/user', userRoute)
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Backend server is running')
+})
