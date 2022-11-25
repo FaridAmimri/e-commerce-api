f4972d7d47cd2373cd96f97629017a44140e9ced
Farid Amimri <farid.amimri@gmail.com>
2022-11-25 11:25:03 +0100

    Implement accessToken with jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 
@@ -41,9 +42,18 @@ router.post('/login', async (req, res) => {
     decryptedPassword !== req.body.password &&
       res.status(401).json('Wrong credentials!')
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin
+      },
+      process.env.SECRET_JWT,
+      { expiresIn: '3d' }
+    )
+
     const { password, ...others } = user._doc
 
-    res.status(200).json(others)
+    res.status(200).json({ ...others, accessToken })
   } catch (error) {
     res.status(500).json(error)
   }
