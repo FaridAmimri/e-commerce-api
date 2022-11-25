ad042b0b72fe2278a4b472c51d90251d4a38cd8e
Farid Amimri <farid.amimri@gmail.com>
2022-11-25 10:57:37 +0100

    Set NodeJs MongoDB authentication for register and login

---
+++ routes/auth.js
@@ -0,0 +1,52 @@
+/** @format */
+
+const router = require('express').Router()
+const User = require('../models/User')
+const CryptoJS = require('crypto-js')
+
+// REGISTER
+
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_PASSWORD.toString()
+    )
+  })
+
+  try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// LOGIN
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    !user && res.status(401).json('Wrong credentials!')
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET_PASSWORD
+    )
+
+    const decryptedPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+    decryptedPassword !== req.body.password &&
+      res.status(401).json('Wrong credentials!')
+
+    const { password, ...others } = user._doc
+
+    res.status(200).json(others)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+module.exports = router
