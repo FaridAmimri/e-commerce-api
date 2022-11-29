0ca54f7f659f8a1f01670cd033ddea68694ba31a
Farid Amimri <farid.amimri@gmail.com>
2022-11-29 12:25:45 +0100

    Add productRoute, cartRoute and orderRoute

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
-dotenv.config()
+dotenv.config() // can not use dotenv without this fn
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,10 +19,15 @@ mongoose
     console.log(error)
   })
 
-app.use(express.json())
+// API Routes
+app.use(express.json()) // Allow json file
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
 
+// If there is no PORT number in env file, use 3000 PORT
 app.listen(process.env.PORT || 3000, () => {
   console.log('Backend server is running')
 })
