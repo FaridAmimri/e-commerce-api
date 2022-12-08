9a0ac5fc3b0a860580eb95cb3351785e0c92567d
Farid Amimri <farid.amimri@gmail.com>
2022-12-08 11:11:56 +0100

    Correction size and color to array

--- models/Product.js
+++ models/Product.js
@@ -20,15 +20,19 @@ const ProductSchema = new mongoose.Schema(
     categories: {
       type: Array
     },
-    size: {
-      type: String
+    sizes: {
+      type: Array
     },
-    color: {
-      type: String
+    colors: {
+      type: Array
     },
     price: {
       type: Number,
       required: true
+    },
+    inStock: {
+      type: Boolean,
+      default: true
     }
   },
   { timestamps: true }
